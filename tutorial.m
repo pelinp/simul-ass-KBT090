ebc77af82aee88f8453a60c125c4ce08e256f769
Elin Persson <elinpersson@MacBook-Pro-som-tillhor-Elin.local>
2022-03-29 17:00:26 +0200

    tis_29

--- tutorial.m
+++ tutorial.m
@@ -1,15 +1,13 @@
-length(x)
-max(x)
-min(x)
-sum(x)
-mean(x)
-size(x)
-disp(f)
-whos
-
 %% Exercise 1
 i=1
 for i=1:10
-
+a(i)=1/i
 end
+summa=sum(a)
 
+%% Exercise 2 
+summa2 = sum(5:514)
+
+%% Exercise 3
+function oddoreven = number(x)
+ for 
